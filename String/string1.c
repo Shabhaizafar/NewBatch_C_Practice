8750ef3387e66b7a23e8cd512fa6bf9804672d5e
Shabhaizafar <zafarshabhai03@gmail.com>
2025-08-26 17:45:06 +0530

    String 1

---
+++ String/string1.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<string.h>
+
+void main(){
+    // 1. pre-defined String :
+    // char mystr[] = "Royal";
+    //index         01234
+
+    // char mystr[] = {"Royal"};
+
+    // char mystr[] = "Royal Techno";
+
+
+
+    // printf("Your String : %s",mystr);
+    // printf("\nValue of 0 index  : %c",mystr[0]);
+    // printf("\nValue of 4 index  : %c",mystr[4]);
+    // printf("\nValue of 4 index  : %c",mystr[5]);
+
+    // printf("\n Size of String  : %d",sizeof(mystr));   // not valid
+    // printf("\n Size/length of String  : %d",strlen(mystr));
+
+    // printf("\nValue of 4 index  : %c",mystr[strlen(mystr)-1]);
+
+
+    // 2. User-defined String :
+    // char firstname[20];
+    // printf("Enter Your Name : ");
+    // // scanf("%s",&firstname);
+    // scanf("%s",firstname);
+
+    // printf("\nYour Name is : %s",firstname);
+
+    /*
+    char fullname[20];
+    printf("Enter Your Name : ");
+    scanf("%s",fullname);
+
+    printf("\nYour Name is : %s",fullname);
+    */
+
+//    char fullname[20];
+//     puts("Enter Your Name : ");
+//     gets(fullname);
+
+//     printf("\nYour Name is : %s",fullname);
+//     printf("\nSizeof : %d",sizeof(fullname));
+//     printf("\nStrLen : %d",strlen(fullname));
+}
+
+
+
+
+/*
+    String  : Royal
+    1. Print All Chars in a New Lines 
+                R
+                o
+                y
+                a
+                l
+
+    2.covert into uppercase
+        input : Royal
+        output: ROYAL 
+    
+    3.covert into lowercase
+        input : Royal
+        output: royal
+
+    4.covert into swapcase
+        input : Royal
+        output: rOYAL
+        
+        
+*/
